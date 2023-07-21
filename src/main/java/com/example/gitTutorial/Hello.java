59dff8c3582f4d7c0b410d173b8c52b918249632
yogesh <yogeshdewade860@gmail.com>
2023-07-21 19:38:33 +0530

    Changed code Line no.from 5-6 in Hello.java

--- src/main/java/com/example/gitTutorial/Hello.java
+++ src/main/java/com/example/gitTutorial/Hello.java
@@ -2,6 +2,8 @@ package com.example.gitTutorial;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Yogesh");
+        for (int i=0;i<10;i++){
+            System.out.println("Hello");
+        }
     }
 }
